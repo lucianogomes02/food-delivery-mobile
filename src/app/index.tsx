b1e687b83886699ca0b362d82f189bbb67afc3ff
Luciano Gomes <lucianogvda02@gmail.com>
2024-08-19 04:29:39 -0300

    feat: implements search field

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,6 +3,7 @@ import Header from "../components/header";
 
 import Constants from "expo-constants";
 import Banner from "../components/banner";
+import Search from "../components/search";
 
 const statusBarHeight = Constants.statusBarHeight;
 
@@ -16,6 +17,7 @@ export default function Index() {
       <View className="w-full px-4" style={{ marginTop: statusBarHeight + 8 }}>
         <Header />
         <Banner />
+        <Search />
       </View>
     </ScrollView>
   );
