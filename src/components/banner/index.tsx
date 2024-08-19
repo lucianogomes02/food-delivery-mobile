6a72a6bf5aeb7c6bfa21a2e29f8c5e8659164c7d
Luciano Gomes <lucianogvda02@gmail.com>
2024-08-19 04:22:16 -0300

    feat: implements banner

--- src/components/banner/index.tsx
+++ src/components/banner/index.tsx
@@ -1,9 +1,32 @@
-import { View, Text } from 'react-native';
+import { Pressable, View, Image } from 'react-native';
+import PagerView from 'react-native-pager-view';
 
 export default function Banner() {
  return (
-   <View>
-        <Text> Banner </Text>
+   <View className="w-full h-36 md:h-60 rounded-2xl mt-5 mb-4">
+        <PagerView style={{ flex: 1 }} initialPage={0} pageMargin={14}>
+            <Pressable 
+                className="w-full h-36 md:h-60 rounded-2xl"
+                key="1"
+                onPress={() => console.log("Pressed Banner 1")}
+            >
+                <Image 
+                    source={require("../../assets/banner1.png")}
+                    className="w-full h-36 md:h-60 rounded-2xl"
+                />
+            </Pressable>
+
+            <Pressable 
+                className="w-full h-36 md:h-60 rounded-2xl"
+                key="2"
+                onPress={() => console.log("Pressed Banner 1")}
+            >
+                <Image 
+                    source={require("../../assets/banner2.png")}
+                    className="w-full h-36 md:h-60 rounded-2xl"
+                />
+            </Pressable>
+        </PagerView>
    </View>
   );
 }
