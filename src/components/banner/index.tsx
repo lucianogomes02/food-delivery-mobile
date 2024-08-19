850122cbe00fb2d7f2880cb921fe4f3876f6349d
Luciano Gomes <lucianogvda02@gmail.com>
2024-08-19 04:06:40 -0300

    feat: add header and init banner

---
+++ src/components/banner/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function Banner() {
+ return (
+   <View>
+        <Text> Banner </Text>
+   </View>
+  );
+}
---
+++ src/components/header/index.tsx
@@ -0,0 +1,25 @@
+import { Text, View, Pressable } from 'react-native';
+import {Ionicons, Feather }from '@expo/vector-icons';
+
+export default function Header() {
+    return (
+        <View className="w-full flex flex-row items-center justify-between">
+            <Pressable className="w-10 h-10 bg-white rounded-full flex justify-center items-center">
+                <Ionicons name="menu" size={20} color="black" />
+            </Pressable>
+
+            <View className="flex flex-col items-center justify-center">
+                <Text className="text-center text-sm"> Localização </Text>
+
+                <View className="flex-row items-center justify-center gap-1">
+                    <Feather name="map-pin" size={14} color="#FF0000" />
+                    <Text className="text-lg font-bold"> São Paulo, SP </Text>
+                </View>
+            </View>
+
+            <Pressable className="w-10 h-10 bg-white rounded-full flex justify-center items-center">
+                <Feather name="bell" size={20} color="black" />
+            </Pressable>
+        </View>
+    )
+}
